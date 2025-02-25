2cb0eb7948d2c8bac0fc0ec6c34337ca817a691d
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-25 01:14:19 +0100

    [feat] import pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 // Integro il sistema di routing
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import ListProduct from "./pages/ListProduct";
 
 import './App.css'
 
 function App() {
 
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/ListProduct" element={<ListProduct />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
 
 }
 
