e28c22cdb7b89fd3f7bff2f351ef06f35d8a03a3
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-25 01:13:42 +0100

    [feat] created pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+// HOMEPAGE 
+export default function Home() {
+    return (
+        <h1>Questa è l'homepage</h1>
+    )
+}
---
+++ src/pages/ListProduct.jsx
@@ -0,0 +1,5 @@
+export default function ListProduct() {
+    return (
+        <h1>Questa è lla lista dei prodotti</h1>
+    )
+}
