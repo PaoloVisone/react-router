9c4d442aab66520e70a7e259c953ecad2dd4ded8
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-26 13:55:30 +0100

    [feat] add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar"
+
+export default function Header(props) {
+
+    // dati ricavati per la gestione dei link della Navbar
+    const links = [
+        { id: 1, text: 'Home', url: '/', current: true },
+        { id: 2, text: 'About', url: '/AboutUs', current: false },
+        { id: 3, text: 'ListProduct', url: '/ListProduct', current: false },
+    ];
+
+
+    return (
+        <header>
+            <Navbar linksProp={links} />
+        </header>
+    )
+}
