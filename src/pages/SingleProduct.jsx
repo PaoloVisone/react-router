ea05438e2c9a09e308c0c312d01e9f126aeaf505
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-26 17:44:55 +0100

    [fix] nav for post single page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -32,15 +32,15 @@ export default function SingleProduct() {
 
             {/* Navigazione tra posts */}
             <nav>
-                <Link to={`/posts/${parseInt(id) - 1}`}>+</Link>
-                <Link to={`/posts/${parseInt(id) + 1}`}>-</Link>
+                <Link className="arrow" to={`/posts/${parseInt(id) - 1}`}>&#8592;</Link>
+                <Link className="arrow" to={`/posts/${parseInt(id) + 1}`}>&#8594;</Link>
             </nav>
 
             {/* Dettagli */}
             <h2>{post.title}</h2>
             <img src={post.image} alt={post.title} />
             <p>{post.content}</p>
-            <span>{post.tags.join(", ")}</span>
+            {/* <span>{post.tags.join(", ")}</span> */}
 
         </>
     );
